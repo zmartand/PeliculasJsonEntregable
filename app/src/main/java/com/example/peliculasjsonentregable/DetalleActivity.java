85b0ad115dd6dc6ef130f08c1e1139c2f4769561
Zara Martín <zmartand@myuax.es>
2023-10-25 20:34:54 +0200

    Actividad detalle

--- app/src/main/java/com/example/peliculasjsonentregable/DetalleActivity.java
+++ app/src/main/java/com/example/peliculasjsonentregable/DetalleActivity.java
@@ -2,7 +2,12 @@ package com.example.peliculasjsonentregable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetalleActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class DetalleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
+
+        Intent intent = getIntent();
+        Pelicula pelicula = (Pelicula) intent.getSerializableExtra("pelicula");
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView tituloTextView = findViewById(R.id.tituloTextView);
+        TextView directorTextView = findViewById(R.id.directorTextView);
+        TextView actoresTextView = findViewById(R.id.actoresTextView);
+
+        Glide.with(this).load(pelicula.getPortada()).into(imageView);
+        tituloTextView.setText("Título: " + pelicula.getTitulo());
+        directorTextView.setText("Director: " + pelicula.getDirector());
+        actoresTextView.setText("Actores: " + String.join(", ", pelicula.getActores()));
+
+
+
     }
 }
