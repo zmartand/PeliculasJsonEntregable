9e22577cb6b0069a7f5b2a10ab425c283b3a92a4
Zara Martín <zmartand@myuax.es>
2023-10-25 20:04:43 +0200

    atributos de Pelicula

--- app/src/main/java/com/example/peliculasjsonentregable/Pelicula.java
+++ app/src/main/java/com/example/peliculasjsonentregable/Pelicula.java
@@ -1,4 +1,14 @@
 package com.example.peliculasjsonentregable;
 
+import java.util.List;
+
 public class Pelicula {
+    private String titulo;
+    private String director;
+    private List<String> actores;
+    private String portada;
+
+
+
+
 }
