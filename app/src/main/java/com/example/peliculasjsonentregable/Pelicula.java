83078b2eef84c8ca61ecd817344c5d3744b22df3
Zara Martín <zmartand@myuax.es>
2023-10-25 20:05:29 +0200

    metodos de clase Pelicula

--- app/src/main/java/com/example/peliculasjsonentregable/Pelicula.java
+++ app/src/main/java/com/example/peliculasjsonentregable/Pelicula.java
@@ -8,7 +8,42 @@ public class Pelicula {
     private List<String> actores;
     private String portada;
 
+    public Pelicula(String titulo, String director, List<String> actores, String portada) {
+        this.titulo = titulo;
+        this.director = director;
+        this.actores = actores;
+        this.portada = portada;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public List<String> getActores() {
+        return actores;
+    }
+
+    public void setActores(List<String> actores) {
+        this.actores = actores;
+    }
+
+    public String getPortada() {
+        return portada;
+    }
+
+    public void setPortada(String portada) {
+        this.portada = portada;
+    }
 }
